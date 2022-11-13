4a29f45093e3ffda7189b084bf37341d0cf2288e
vickanloly <viktoria7lizogub@gmail.com>
2022-11-13 11:37:00 +0200

    add js

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    navText: [ '', ' ' ],
+    autoplay: true,
+    autoplayTimeout:2000,
+    responsive:{
+        0:{
+            items:2
+        },
+
+        1000:{
+            items:3
+        }
+    }
+});
+});
